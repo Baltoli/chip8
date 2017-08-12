c7a264813d8b32938073d748210796044f06a036
Bruce Collie <bsc28@cam.ac.uk>
2017-08-12 18:47:58 +0100

    Restructure

--- src/chip8/src/app.c
+++ src/chip8/src/app.c
@@ -8,22 +8,16 @@
 #define SCREEN_WIDTH (64*5)
 #define SCREEN_HEIGHT (32*5)
 
-int main(int argc, char **argv)
-{ 
-  struct c8_interpreter *in = new_interpreter();
+static SDL_Window *window;
 
-  in->cpu.memory[0x200] = 0x12;
-  in->cpu.memory[0x201] = 0x00;
-
-  in->running = true;
-
-  SDL_Window* window = NULL;
-
+void init_graphics(void)
+{
   if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-    printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    exit(1);
   } else {
     window = SDL_CreateWindow(
-      "SDL Tutorial", 
+      "Chip8", 
       SDL_WINDOWPOS_UNDEFINED, 
       SDL_WINDOWPOS_UNDEFINED, 
       SCREEN_WIDTH, 
@@ -32,45 +26,61 @@ int main(int argc, char **argv)
     );
 
     if(window == NULL) {
-      printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-    } else {
-      bool quit = false;
-      SDL_Event e;
-      SDL_Renderer* gRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-      if(!gRenderer) {
-        printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-        return 1;
-      }
-
-      uint32_t last_draw = SDL_GetTicks();
-
-      while(!quit && in->running) {
-        step(in);
-
-        while(SDL_PollEvent( &e ) != 0) {
-          if( e.type == SDL_QUIT ) {
-            quit = true;
-          }
-        }
-
-        if(SDL_GetTicks() - last_draw >= 16) {
-          for(int y = 0; y < 32; ++y) {
-            for(int x = 0; x < 64; ++x) {
-              int colour = in->display.buffer[y*64 + x] ? 0xFF : 0x00;
-
-              SDL_Rect fillRect = { x*5, y*5, 5, 5 };
-              SDL_SetRenderDrawColor( gRenderer, colour, colour, colour, 0xFF );
-              SDL_RenderFillRect( gRenderer, &fillRect );
-            }
-          }
-          last_draw = SDL_GetTicks();
-        }
-
-        SDL_RenderPresent( gRenderer );
-      }
+      fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+      exit(1);
     }
   }
+}
+
+void emulator_loop(struct c8_interpreter *in)
+{
+  bool quit = false;
+  SDL_Event e;
+  SDL_Renderer* gRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if(!gRenderer) {
+    printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+  }
+
+  uint32_t last_draw = SDL_GetTicks();
+
+  while(!quit && in->running) {
+    step(in);
+
+    while(SDL_PollEvent( &e ) != 0) {
+      if( e.type == SDL_QUIT ) {
+        quit = true;
+      }
+    }
+
+    if(SDL_GetTicks() - last_draw >= 16) {
+      for(int y = 0; y < 32; ++y) {
+        for(int x = 0; x < 64; ++x) {
+          int colour = in->display.buffer[y*64 + x] ? 0xFF : 0x00;
+
+          SDL_Rect fillRect = { x*5, y*5, 5, 5 };
+          SDL_SetRenderDrawColor( gRenderer, colour, colour, colour, 0xFF );
+          SDL_RenderFillRect( gRenderer, &fillRect );
+        }
+      }
+      last_draw = SDL_GetTicks();
+    }
+
+    SDL_RenderPresent( gRenderer );
+  }
+}
+
+int main(int argc, char **argv)
+{ 
+  init_graphics();
+  struct c8_interpreter *in = new_interpreter();
+
+  in->cpu.memory[0x200] = 0x12;
+  in->cpu.memory[0x201] = 0x00;
+
+  in->running = true;
   
+  emulator_loop(in);
+
   SDL_DestroyWindow( window );
   SDL_Quit();
   
