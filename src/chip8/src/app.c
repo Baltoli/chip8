51c502baf0f4da5ff6b152abc5d947bde6a1d897
Bruce Collie <bsc28@cam.ac.uk>
2017-08-12 15:34:20 +0100

    Use SDL

--- src/chip8/src/app.c
+++ src/chip8/src/app.c
@@ -1,44 +1,15 @@
 #include "interpreter.h"
 
-#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void error_callback(int error, const char* description)
-{
-    fprintf(stderr, "Error: %s\n", description);
-}
-
-GLFWwindow *init_graphics(void)
-{
-  glfwSetErrorCallback(error_callback);
-
-  if(!glfwInit()) {
-    return NULL;
-  }
-
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-
-  glfwMakeContextCurrent(window);
-  glfwSwapInterval(1);
-
-  return window;
-}
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
 int main(int argc, char **argv)
 { 
-  GLFWwindow *wind = init_graphics();
-  if(!wind) {
-    glfwTerminate();
-    return 1;
-  }
-
   struct c8_interpreter *in = new_interpreter();
 
   in->cpu.memory[0x200] = 0x60;
@@ -47,12 +18,43 @@ int main(int argc, char **argv)
   run(in);
   dump_state(in);
 
-  /*while (!glfwWindowShouldClose(wind)) {
-    glfwSwapBuffers(wind);
-    glfwPollEvents();
-  }*/
+  SDL_Window* window = NULL;
+  SDL_Surface* screenSurface = NULL;
 
-  glfwDestroyWindow(wind);
-  glfwTerminate();
+  if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+  } else {
+    window = SDL_CreateWindow(
+      "SDL Tutorial", 
+      SDL_WINDOWPOS_UNDEFINED, 
+      SDL_WINDOWPOS_UNDEFINED, 
+      SCREEN_WIDTH, 
+      SCREEN_HEIGHT, 
+      SDL_WINDOW_SHOWN
+    );
+
+    if(window == NULL) {
+      printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+    } else {
+      //Get window surface
+      screenSurface = SDL_GetWindowSurface( window );
+
+      //Fill the surface white
+      SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+      
+      //Update the surface
+      SDL_UpdateWindowSurface( window );
+
+      //Wait two seconds
+      for(int i = 0; i < 2000; i++){
+        SDL_PumpEvents();
+        SDL_Delay(1);
+      }
+    }
+  }
+  
+  SDL_DestroyWindow( window );
+  SDL_Quit();
+  
   return 0;
 }
